aeddb9d52d5ee79fca7f4e6a4b70153f5ae68883
Lwildd <lena.wild49@gmail.com>
2025-09-08 09:28:57 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { CssBaseline, Container } from '@mui/material';
+import Header from './Header';
+import About from './Sections/About';
+import Education from './Sections/Education';
+import Publications from './Sections/Publications.js';
+import Experience from './Sections/Experience';
+import Footer from './Footer.jsx';
+import { BrowserRouter } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+    <BrowserRouter basename="">
+  
+      <CssBaseline />
+      <Header />
+      <Container maxWidth="md" sx={{ mt: 10 }}>
+        <About />
+        <Publications />
+        <Education />
+        <Experience />
+      <Footer/>
+      </Container>
+
+</BrowserRouter>
+    </>
+  );
+}
+
+export default App;
